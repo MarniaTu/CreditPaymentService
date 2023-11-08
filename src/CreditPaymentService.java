1d36fb3cf2e3c5732541c865cf9db19decd0d36e
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2023-11-08 14:25:39 +0300

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,10 @@
+public class CreditPaymentService {
+    public int calculate (int sum, int periods) {
+        double percentMonthly = 9.99 / 100 / 12;
+        double ratio;
+        double c = Math.pow(1 + percentMonthly, periods);
+        ratio = percentMonthly * c / (c - 1);
+        double amount = sum * ratio;
+        return (int) amount;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+
+        System.out.println();
+        int creditForOneYear = service. calculate(1_000_000, 12);
+        System.out.println("Ежемесячный платеж составит: " + creditForOneYear + " рубль(ля/лей)");
+
+        System.out.println();
+        int creditForTwoYears = service.calculate(1_000_000, 24);
+        System.out.println("Ежемесячный платеж составит: " + creditForTwoYears + " рубль(ля/лей)");
+
+        System.out.println();
+        int creditForThreeYears = service.calculate(1_000_000, 36);
+        System.out.println("Ежемесячный платеж составит: " + creditForThreeYears + " рубль(ля/лей)");
+    }
+}
